deb4ddc323441452570742bc7da0ca801abb838b
Edgar Martinez Flores <emf080996@gmail.com>
2018-11-28 02:57:58 -0600

    1.Ingresar mas de un producto por cliente 2.Eliminar un producto antes de realizar la compra 3.Restar cantidad de un producto antes de terminar la compra 4.Muestra el total de la compra del cliente 5.Asigna una clave a los productos automaticamente 6.Asingna un folio a cada cliente que finalice la compra para des pues poderlo buscar con ese numero de folio y mostrar sus compras 7.Permite cancelar la compra antes de registrarla en un nuevo nodo 8.Muestra el total en caja 9.Muestra los productos ordenados por clave 10.Solo al finalizar la compra se hacen los cambios en los nodos de otro modo solo son el la estructura 11.Verifica si en producto esta disponible 12.No se agregar al carrito si el producto es menor a la cantidad requerida 12.No asigna folio al cliente antes de finalizar la compra 13.Si se le resta a un producto una cntidad que sea igual a la que se tenia en el carrito este producto se elimina

--- tiendaE.cpp
+++ tiendaE.cpp
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 using namespace std;
+
+const int NUM=20;
 //estructura tienda 
 struct Tienda{
-	char clave[20];//clave del producto 
-	char nombre[20];//nombre del producto
+	int clave;//clave del producto 
+	string nombre;//nombre del producto
 	int cantidad;//cantidad de producto
 	double precio;//precio del producto
 };
@@ -19,34 +21,50 @@ struct Nodo{
 };
 
 struct Cliente{
+	int folio;
 	double total;
-	string clave[20];
-	string nombre[20];
-	double precio;
+	int claveP[NUM];
+	string nombre[NUM];
+	double precio[NUM];
+	int cantidad[NUM];
 };
 
 struct Nodo2{
-	Nodo2 *lista;
+	Nodo2 *siguiente;
 	Cliente carrito;
 };
 
 void menu();
 void menu2();
-void agregarProducto(Tienda &);
+void agregarProducto(Tienda &,int &);
 void insertarProducto(Nodo *&,Tienda);
 void venta(Nodo *&,int &);
 void imprimirDatos(Nodo *&);
 Nodo *nDinamico(Tienda);
+bool buscar(Nodo *,int,int &);
+void agregarCarrito(Nodo *,Cliente &,int,int);
+void eliminarProducto(Cliente &,int);
+void insertarCliente(Nodo2 *&,Cliente,int,double &);
+void restar(Cliente &,int,int);
+void cancelar(Cliente &);
+void mostrarCarrito(Cliente &);
+void mostrarCliente(Nodo2 *&,int &);
+void rellenar(Cliente &);
 
 int main(){
 	Nodo *arbol=NULL;
+	Nodo2 *lista=NULL;
+	Cliente carrito;
 	Tienda producto;
 	char opcion;//variable para el switch
 	double caja=0;
-	char clave[20];
+	int clave;
 	char opcion2;
-	double totalC;
 	int cantidad;
+	int claveP=1;
+	int auxCantidad=0;
+	int claveC;
+	int auxFolio=1;
 	
 	do{
 		menu();
@@ -54,37 +72,66 @@ int main(){
 		fflush(stdin);//vacia el buffer
     	switch(opcion){
 	    	case '1':
-		    	agregarProducto(producto);//llamando a funcion para agregar productos
+		    	agregarProducto(producto,claveP);//llamando a funcion para agregar productos
 		    	insertarProducto(arbol,producto);//llamando funcion para insertar los nodos a la cola
+		    	claveP +=1;
 			    break;
     		case '2':
+    			rellenar(carrito);
     			do{
+    				Nodo *auxArbol=NULL;
+    				auxArbol = arbol;
     				menu2();
     				cin>>opcion2;
     				fflush(stdin);
     				switch(opcion2){
     					case '1':
-						    if(stock(clave,arbol)){
-						    	agregarCarrito();
+    						cout<<"\nIngresar clave del producto: "; cin>>claveC;
+						    if(buscar(auxArbol,claveC,auxCantidad) == true){
+						    	cout<<"\nCantidad de producto disponible: "<<auxCantidad<<endl;
+						    	cout<<"\nIngresar la cantidad de produto a vender: "; cin>>cantidad;
+						    	if(cantidad<=auxCantidad){
+						    		agregarCarrito(auxArbol,carrito,claveC,cantidad);
+						    		
+								}else{
+									cout<<"\nProducto en stock insuficiente."<<endl;
+								}
+							}else{
+								cout<<"\nError."<<endl;
 							}
+							//mostrarCarrito(carrito);
 							break;
     					case '2':
+    						//mostrarCarrito(carrito);
+    						cout<<"\nIngresar clave del producto a quitar: "; cin>>claveC;
+    						eliminarProducto(carrito,claveC);
 						    break;
     					case '3':
-    						cout<<"\nIngresar la clave del producto: "; cin.getline(clave,20,'\n');
+    						//mostrarCarrito(carrito);
+    						cout<<"\nIngresar la clave del producto: "; cin>>clave;
     						fflush(stdin);
     						cout<<"\nIngresar cantidad a restar del producto: "; cin>>cantidad;
+    						restar(carrito,clave,cantidad);
 						    break;
     					case '4': 
-						    //
+    					    //mostrarCarrito(carrito);
+						    if(carrito.claveP[0] !=0){
+						    	insertarCliente(lista,carrito,auxFolio,caja);
+							}else{
+								cout<<"\nCarrito vacio."<<endl;
+							}
 						    break;
     					case '5': 
-					    	//
+					    	cancelar(carrito);
+					    	cout<<"\nVenta cancelada"<<endl;
 						    break;
+						case '6':
+							cout<<"\nSalio de ventas, su carrito se guardara hasta que cancele la compra."<<endl;
+							break;
     					defalut:  //
 						    break;
 					}
-				}while(opcion2 !='5'&&opcion2 !='6');
+				}while(opcion2 !='5'&&opcion2 !='6'&&opcion2 !='4');
     			
 		    	break;
     		case '3':
@@ -93,7 +140,11 @@ int main(){
 				cout<<"\nDinero en caja: "<<caja;//imprime dinero en caja
 		    	break;
    	        case '4':
-   	        	cout<<"Salio del programa..."<<endl;
+   	        	cout<<"\nIngresar folio del cliente: "; cin>>claveC;
+   	        	mostrarCliente(lista,claveC);
+   	        	break;
+   	        case '5':
+   	        	cout<<"\nSalio del programa correctamente."<<endl;
    	        	break;
       	    default:
       	    	cout<<"\nOpcion invalida."<<endl;
@@ -101,7 +152,7 @@ int main(){
     	}
     	getch();//pausa para evitar que se limpie pantalla sin que el usuario la vea
     	system("cls");//Limpia pantalla
-    }while(opcion !='4');
+    }while(opcion !='5');
 	return 0;//retorna un 0 si no hay errores
 }
 
@@ -111,7 +162,8 @@ void menu(){
 	    <<"1.Ingresar producto."<<endl
 	    <<"2.Vender producto."<<endl
 	    <<"3.Mostrar productos."<<endl
-	    <<"4.Salir"<<endl
+	    <<"4.Buscar registro de un cliente."<<endl
+	    <<"5.Salir"<<endl
 	    <<"Ingresar una opcion del menu: ";
 }
 
@@ -122,6 +174,7 @@ void menu2(){
 	    <<"3.Restar cantidad de un producto."<<endl
 	    <<"4.Finalizar venta."<<endl
 	    <<"5.Cancelar venta."<<endl
+	    <<"6.Salir de ventas."<<endl
 	    <<"Ingresar opcion: ";
 }
 
@@ -136,9 +189,9 @@ Nodo *nDinamico(Tienda producto){
 }
 
 //Pide datos del producto 
-void agregarProducto(Tienda &producto){
-	cout<<"Ingresar clave del producto: "; cin.getline(producto.clave,20,'\n');
-    cout<<"\nIngresar nombre del producto: "; cin.getline(producto.nombre,25,'\n');
+void agregarProducto(Tienda &producto,int &claveP){
+	producto.clave=claveP;
+    cout<<"\nIngresar nombre del producto: "; cin>>producto.nombre;
    	cout<<"\nIngresar precio del producto: "; cin>>producto.precio;
    	cout<<"\nIngresar cantidad del producto: "; cin>>producto.cantidad;
 }
@@ -149,7 +202,7 @@ void insertarProducto(Nodo *&arbol ,Tienda producto){
 	Nodo *nNodo = nDinamico(producto);
 	arbol = nNodo;
 	}else{
-		if(producto.precio < arbol->producto.precio){
+		if(producto.clave < arbol->producto.clave){
 			insertarProducto(arbol->izquierdo,producto);
 		}else{
 			insertarProducto(arbol->derecho,producto);
@@ -163,9 +216,153 @@ void imprimirDatos(Nodo *&arbol){
 	return;
 	}else{
 		imprimirDatos(arbol->derecho);
-		cout<<arbol->producto.clave<<"\t"<<arbol->producto.nombre<<"\t\t"<<arbol->producto.precio<<"\t\t"<<arbol->producto.cantidad;
+		cout<<arbol->producto.clave<<"\t"<<arbol->producto.nombre<<"\t\t"<<arbol->producto.precio<<"\t\t"<<arbol->producto.cantidad<<endl;
 		imprimirDatos(arbol->izquierdo);
 	}
 }
 
-bool stock()
+bool buscar(Nodo *arbol,int clave,int &auxCantidad){
+	
+	if(arbol ==NULL){
+		return false;
+	}else if(arbol->producto.clave==clave){
+		auxCantidad=arbol->producto.cantidad;
+		return true;
+	}else if(clave < arbol->producto.clave){
+		return buscar(arbol->izquierdo,clave,auxCantidad);
+	}else{
+		return buscar(arbol->derecho,clave,auxCantidad);
+	}
+}
+
+void agregarCarrito(Nodo *arbol,Cliente &carrito,int clave,int cantidad){
+	int auxClave = arbol->producto.clave;
+	if(arbol ==NULL){
+	    return;
+	}else if(arbol->producto.clave==clave){
+		cantidad=arbol->producto.cantidad;
+		for(int i=0; i<NUM; i++){
+		if(carrito.claveP[i]==-1){
+			carrito.claveP[i]=arbol->producto.clave;
+			carrito.nombre[i]=arbol->producto.nombre;
+			carrito.precio[i]=arbol->producto.precio;
+			carrito.cantidad[i]=cantidad;
+			carrito.total=carrito.total+(arbol->producto.precio*cantidad);
+			return;
+		}
+	}
+	}else if(clave < auxClave){
+		agregarCarrito(arbol->izquierdo,carrito,clave,cantidad);
+	}else{
+		agregarCarrito(arbol->derecho,carrito,clave,cantidad);
+	}
+}
+
+void eliminarProducto(Cliente &carrito,int claveC){
+	for(int i=0; i<NUM; i++){
+		if(carrito.claveP[i]==claveC){
+			carrito.claveP[i]=-1;
+			carrito.nombre[i]='0';
+			carrito.total=carrito.total-(carrito.precio[i]*carrito.cantidad[i]);
+			carrito.precio[i]=0;
+			carrito.cantidad[i]=0;
+			cout<<"\nEl producto ha sido eliminado"<<endl;
+			return;
+		}
+	}
+	cout<<"\nProducto no encontrado."<<endl;
+}
+
+void insertarCliente(Nodo2 *&lista,Cliente carrito,int auxFolio,double &caja){
+	carrito.folio=auxFolio;
+	caja +=carrito.total;
+	Nodo2 *nNodo = new Nodo2;
+	nNodo->carrito = carrito;
+	Nodo2 *aux1 = lista;
+	Nodo2 *aux2;
+	while((aux1 !=NULL)&&(aux1->carrito.claveP<carrito.claveP)){
+		aux2 = aux1;
+		aux1 = aux1->siguiente;
+	}
+	if(lista == aux1){
+		lista = nNodo;
+	}else{
+		aux2->siguiente = nNodo;
+	}
+	nNodo->siguiente = aux1;
+	cout<<carrito.folio;
+	auxFolio +=1;
+}
+
+void restar(Cliente &carrito,int clave,int cantidad){
+	for(int i=0; i<NUM; i++){
+		if(carrito.claveP[i]==clave){
+			if(carrito.cantidad[i]<cantidad){
+				carrito.total=carrito.total-(carrito.precio[i]*cantidad);
+				carrito.cantidad[i]=carrito.cantidad[i]-cantidad;
+				return;
+			}else if(carrito.cantidad[i]==cantidad){
+				eliminarProducto(carrito,clave);
+				return;
+			}else{
+				cout<<"\nLa cantidad ingresada es mayor a la cantidad de producto en el carrito"<<endl;
+				return;
+			}
+		}
+	}
+}
+
+void cancelar(Cliente &carrito){
+	for(int i=0; i<20; i++){
+		carrito.cantidad[i]=0;
+		carrito.claveP[i]=0;
+		carrito.nombre[i]='0';
+		carrito.precio[i]=0;
+	}
+	carrito.total=0;
+}
+
+void mostrarCarrito(Cliente &carrito){
+	int x=0;
+	cout<<"CLAVE\t"<<"NOMBRE\t\t"<<"PRECIO\t\t"<<"CANTIDAD"<<endl;
+	while(carrito.claveP[x] !=-1){
+		cout<<carrito.claveP[x]<<"\t"<<carrito.nombre<<"\t\t"<<carrito.precio<<"\t\t"<<carrito.cantidad<<endl;
+		x +=1;
+	}
+	if(x !=0) cout<<"\nTOTAL: "<<carrito.total<<endl;
+}
+
+void mostrarCliente(Nodo2 *&lista,int &clave){
+	Nodo2 *auxlista = new Nodo2();
+	auxlista=lista;
+	if(auxlista==NULL){
+		cout<<"\nSin ningun registro"<<endl;
+		return;
+	}else{
+		while((auxlista !=NULL)&&(auxlista->carrito.folio !=clave)){
+			auxlista=auxlista->siguiente;
+		}
+		if(auxlista==NULL){
+			cout<<"\nNo se encontro el registro."<<endl;
+			return;
+		}else{
+			int x=0;
+			cout<<"\nFOLIO DEL CLIENTE: "<<auxlista->carrito.folio<<endl;
+			cout<<"CLAVE\t"<<"NOMBRE\t\t"<<"PRECIO\t\t"<<"CANTIDAD"<<endl;
+			for(int i=0; i<NUM; i++){
+				if(auxlista->carrito.claveP[i] !=-1){
+					cout<<auxlista->carrito.claveP[i]<<"\t"<<auxlista->carrito.nombre[i]<<"\t\t"<<auxlista->carrito.precio[i]<<"\t\t"<<auxlista->carrito.cantidad[i]<<endl;
+				}
+			}
+			cout<<"\nTOTAL DE COMPRA: "<<auxlista->carrito.total<<endl;
+			return;
+		}
+	}
+	
+}
+
+void rellenar(Cliente &carrito){
+	for(int i=0; i<20; i++){
+		carrito.claveP[i]=-1;
+	}
+}
